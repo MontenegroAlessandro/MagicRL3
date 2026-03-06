06fcc32ac9cccd0398f9116842bc945e98fba660
Alessandro Montenegro <73550926+MontenegroAlessandro@users.noreply.github.com>
2026-03-06 23:48:10 +0100

    new config + learn script

--- examples/learn_test.py
+++ examples/learn_test.py
@@ -3,50 +3,82 @@ from stable_baselines3 import A2C, PPO
 from stable_baselines3.common.env_util import make_vec_env
 import wandb
 from wandb.integration.sb3 import WandbCallback
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
-# parameters
-config = dict(
-    policy_type="MlpPolicy",
-    total_timesteps=3_000_000,
-    env_id="HalfCheetah-v5",
-    n_envs=6,
-    dir_name="results",
-)
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from algorithms.rt_ppo import RT_PPO
+from buffers.buffers import MultiRolloutBuffer
 
-# logger 
-run = wandb.init(
-    project="sb3-ppo", 
-    config=config, 
-    sync_tensorboard=True
-)
 
-# make the env
-env = make_vec_env(config["env_id"], n_envs=config["n_envs"], seed=2026)
+@hydra.main(version_base=None, config_path=".", config_name="conf")
+def main(cfg: DictConfig):
+    exp = cfg.experiment
 
-# learn
-model = A2C(
-    config["policy_type"], 
-    env, 
-    verbose=0, 
-    tensorboard_log=f"{config['dir_name']}/runs/{run.id}",
-    seed=2026
-)
-model.learn(
-    total_timesteps=config["total_timesteps"], 
-    progress_bar=True,
-    callback=WandbCallback(
-        model_save_path=f"{config['dir_name']}/models/{run.id}", 
-        verbose=2
+    # logger
+    run = wandb.init(
+        project=cfg.wandb.project,
+        config=OmegaConf.to_container(cfg, resolve=True),
+        sync_tensorboard=cfg.wandb.sync_tensorboard,
     )
-)
-model.save(f"{config['dir_name']}/ppo_halfcheetah")
 
-# evaluate
-eval_env = gym.make(config["env_id"], render_mode="human")
-obs, info = eval_env.reset()
-for i in range(1000):
-    action, _state = model.predict(obs, deterministic=True)
-    obs, reward, terminated, truncated, info = eval_env.step(action)
-    if terminated or truncated:
-        obs, info = eval_env.reset()
-eval_env.close()
+    # make the env
+    env = make_vec_env(exp.env_name, n_envs=exp.n_envs, seed=exp.seed)
+
+    # learn
+    model = RT_PPO(
+        on_policy_critic=exp.on_policy_critic,
+        rollout_buffer_class=MultiRolloutBuffer,
+        rollout_buffer_kwargs=dict(
+            window_size=exp.window_size,
+        ),
+        # Old PPO args
+        policy=exp.policy_type,
+        env=env,
+        learning_rate=exp.learning_rate,
+        n_steps=exp.n_steps,
+        batch_size=exp.batch_size,
+        n_epochs=exp.n_epochs,
+        gamma=exp.gamma,
+        gae_lambda=exp.gae_lambda,
+        clip_range=exp.clip_range,
+        clip_range_vf=exp.clip_range_vf,
+        normalize_advantage=exp.normalize_advantage,
+        ent_coef=exp.ent_coef,
+        vf_coef=exp.vf_coef,
+        max_grad_norm=exp.max_grad_norm,
+        use_sde=exp.use_sde,
+        sde_sample_freq=exp.sde_sample_freq,
+        target_kl=exp.target_kl,
+        stats_window_size=exp.stats_window_size,
+        policy_kwargs=exp.policy_kwargs,
+        verbose=exp.verbose,
+        tensorboard_log=f"{exp.dir_name}/runs/{run.id}",
+        seed=exp.seed,
+        device=exp.device
+    )
+    model.learn(
+        total_timesteps=int(exp.total_timesteps),
+        progress_bar=True,
+        callback=WandbCallback(
+            model_save_path=f"{exp.dir_name}/models/{run.id}",
+            verbose=2,
+        ),
+    )
+    model.save(f"{exp.dir_name}/ppo_halfcheetah")
+
+    # evaluate
+    eval_env = gym.make(exp.env_name, render_mode="human")
+    obs, info = eval_env.reset()
+    for i in range(1000):
+        action, _state = model.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, info = eval_env.step(action)
+        if terminated or truncated:
+            obs, info = eval_env.reset()
+    eval_env.close()
+
+
+if __name__ == "__main__":
+    main()
