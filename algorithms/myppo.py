6aa49bfd75aa2f3e29d763318550ca9f8da65183
Alessandro Montenegro <73550926+MontenegroAlessandro@users.noreply.github.com>
2026-04-24 23:45:44 +0200

    final diag myppo

--- algorithms/myppo.py
+++ algorithms/myppo.py
@@ -14,11 +14,8 @@ class MyPPO(PPO):
         clip_range = self.clip_range(self._current_progress_remaining)
 
         clip_fracs = []
-        naive_is_max = []
-        naive_is_mean = []
-        naive_ess_sum_w  = 0.0
-        naive_ess_sum_w2 = 0.0
-        naive_kl = []
+        kl_vals = []
+        abs_ratio_vals = []
 
         with th.no_grad():
             for rollout_data in self.rollout_buffer.get(batch_size=None):
@@ -28,20 +25,12 @@ class MyPPO(PPO):
                 _, log_prob, _ = self.policy.evaluate_actions(rollout_data.observations, actions)
                 naive_ratio = th.exp(log_prob - rollout_data.old_log_prob)
                 clip_fracs.append((th.abs(naive_ratio - 1) > clip_range).float().mean().item())
-                naive_is_max.append(naive_ratio.max().item())
-                naive_is_mean.append(naive_ratio.mean().item())
-                naive_ess_sum_w  += naive_ratio.sum().item()
-                naive_ess_sum_w2 += (naive_ratio ** 2).sum().item()
-                naive_kl.append(((naive_ratio - 1) - th.log(naive_ratio)).mean().item())
+                kl_vals.append(((naive_ratio - 1) - th.log(naive_ratio)).mean().item())
+                abs_ratio_vals.append((naive_ratio - 1).abs().mean().item())
 
-        ess = naive_ess_sum_w ** 2 / naive_ess_sum_w2 if naive_ess_sum_w2 > 0 else 0.0
-        self.logger.record("diagnostics/clip_fraction_window_0", np.mean(clip_fracs))
-        self.logger.record("diagnostics/clip_fraction_mean", np.mean(clip_fracs))
-        self.logger.record("diagnostics/naive_is_weight_max_window_0", max(naive_is_max))
-        self.logger.record("diagnostics/naive_is_weight_mean_window_0", np.mean(naive_is_mean))
-        self.logger.record("diagnostics/naive_is_weight_max", max(naive_is_max))
-        self.logger.record("diagnostics/naive_is_weight_mean", np.mean(naive_is_mean))
-        self.logger.record("diagnostics/naive_ess_window_0", ess)
-        self.logger.record("diagnostics/naive_ess", ess)
-        self.logger.record("diagnostics/naive_kl_window_0", np.mean(naive_kl))
-        self.logger.record("diagnostics/naive_kl", np.mean(naive_kl))
+        self.logger.record("diagnostics_clip/clip_fraction_window_0", np.mean(clip_fracs))
+        self.logger.record("diagnostics_clip/clip_fraction_mean", np.mean(clip_fracs))
+        self.logger.record("diagnostics_kl/kl_window_0", np.mean(kl_vals))
+        self.logger.record("diagnostics_kl/kl_mean", np.mean(kl_vals))
+        self.logger.record("diagnostics_abs_ratio/final_window_0", np.mean(abs_ratio_vals))
+        self.logger.record("diagnostics_abs_ratio/final_mean", np.mean(abs_ratio_vals))
